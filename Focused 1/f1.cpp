18ef4a74f460e243c7cbe43e6beb3a911a4879ab
Tien Young <tyang2896@conestogac.on.ca>
2024-05-11 16:13:40 -0400

    Implement main function.
    1. Prompt user input.
    2. Fill out  flight information in array of struct.
    3. Print all flight info.

--- Focused 1/f1.cpp
+++ Focused 1/f1.cpp
@@ -18,10 +18,23 @@ int main()
 
 	for (int i = 0; i < 10; i++)
 	{
-		printf("Enter destination:");
+		printf("Flight #%d/10\n", i + 1);
+		printf("Destination:");
+		char buffer[30] = "";
+		fgets(buffer, 30, stdin);
+		char destination[30] = "";
+		sscanf_s(buffer, "%[^\n]s", &destination, 30);
+
 
 		printf("Date:");
+		fgets(buffer, 30, stdin);
+		char date[30] = "";
+		sscanf_s(buffer, "%[^\n]s", &date, 30);
+
+		fillFlightInfo(&flights[i], destination, date);
 	}
+
+	printFlightInfo(flights, 10);
 
 	return 0;
 }
@@ -38,9 +51,9 @@ void fillFlightInfo(FlightInfo* flight, const char* destination, const char* dat
 
 void printFlightInfo(FlightInfo* flights, int size)
 {
-	for (int i = 0; i<size;i++)
+	for (int i = 0; i < size; i++)
 	{
-		printf("Destination: %35s Date: %35s", flights[i].destination, flights[i].date);
+		printf("Destination: %35s Date: %35s\n", flights[i].destination, flights[i].date);
 	}
 }
 
